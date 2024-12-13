053bac2c6631f66e304394362272d2a30f78bdc9
Mohoammad Imran <mohoammadimran@Mohoammads-MacBook-Air.local>
2024-12-13 21:45:20 +0600

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserBankAccountUpdateView, UserRegistrationView, UserLoginView, UserLogoutView
+ 
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('profile/', UserBankAccountUpdateView.as_view(), name='profile' )
+]
